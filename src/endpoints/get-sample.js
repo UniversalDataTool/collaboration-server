4a522a99740db43ed12ecdf2264b1ffb404bbd82
Mohammed Eldadah <mrdadah@gmail.com>
2020-08-06 09:58:35 +0300

    implement get sample api

---
+++ src/endpoints/get-sample.js
@@ -0,0 +1,22 @@
+const { send } = require("micro")
+const cors = require("micro-cors")()
+const error = require("../../utils/error")
+const getSampleObject = require("../../utils/getSampleObject")
+const getDB = require("../db")
+const db = getDB({ databasePath: "udt.db", verbose: null })
+
+module.exports = cors(async (req, res) => {
+  const sessionId = req.params.session_id
+  const sampleIndex = req.params.sample_index
+
+  const sample = db
+    .prepare(
+      `SELECT  *
+       FROM sample_state
+       WHERE session_short_id = ? AND session_sample_index = ?`
+    ).get(sessionId, sampleIndex)
+
+  if (!sample) return error(res, 404, `Sample "${sampleIndex}", Session "${sessionId}" Not Found`)
+
+  return send(res, 200, getSampleObject(sample))
+})
