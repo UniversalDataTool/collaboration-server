364c2fa157ee0d910e1adf9eef60f6c1f2b8d2c1
seveibar <seveibar@gmail.com>
2020-10-15 06:58:49 -0400

    fix: add 404 page

--- src/router.js
+++ src/router.js
@@ -1,3 +1,4 @@
+const micro = require("micro")
 const { router, get, post, patch, options } = require("microrouter")
 
 const getDiffsEndpoint = require("./endpoints/get-diffs.js")
@@ -7,6 +8,8 @@ const getSessionEndpoint = require("./endpoints/get-session.js")
 const getSampleEndpoint = require("./endpoints/get-sample.js")
 const patchSampleEndpoint = require("./endpoints/patch-sample.js")
 const downloadEndpoint = require("./endpoints/download-session.js")
+
+const notfound = (req, res) => micro.send(res, 404)
 
 module.exports = router(
   post("/api/session", postSessionEndpoint),
@@ -18,5 +21,6 @@ module.exports = router(
   patch("/api/session/:session_id/sample/:sample_index", patchSampleEndpoint),
   get("/api/session/:session_id", getSessionEndpoint),
   patch("/api/session/:session_id", patchSessionEndpoint),
-  options("/api/session/:session_id", patchSessionEndpoint)
+  options("/api/session/:session_id", patchSessionEndpoint),
+  get("/*", notfound)
 )
