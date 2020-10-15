62d56ff164776860d9aedae8616e1d9b5177dc48
seveibar <seveibar@gmail.com>
2020-10-15 00:35:34 -0400

    fix: reduce size of large dataset

--- tests/upload-large-dataset.test.js
+++ tests/upload-large-dataset.test.js
@@ -19,7 +19,7 @@ test("Create session", async (t) => {
     samples: [],
   }
 
-  const numSamples = 50000
+  const numSamples = 10000
   for (let i = 0; i < numSamples; i++) {
     udt.samples.push({
       imageUrl:
